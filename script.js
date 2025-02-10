d0216de069789f0df36fc39eb6a4551870e22e9a
AndriCaba <owens5289@gmail.com>
2025-02-11 07:52:28 +0800

    This works HAHAHAHAHAHAHAHHA

--- script.js
+++ script.js
@@ -1,69 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const editBtn = document.getElementById("editBtn");
-    const content = document.getElementById("editableContent");
-    const editIcon = editBtn.querySelector("img");
+function handleImageUpload(inputId, displayId, imageId) {
+    const input = document.getElementById(inputId);
+    const display = document.getElementById(displayId);
+    const image = document.getElementById(imageId);
 
-    editBtn.addEventListener("click", function () {
-        if (content.isContentEditable) {
-            content.contentEditable = "false";
-            editBtn.innerHTML = '<img src="Edit_Icon.svg" alt="EditLogo"> Edit Text';
-        } else {
-            content.contentEditable = "true";
-            editBtn.innerHTML = '<img src="Save_Icon.svg" alt="SaveLogo"> Save';
+    input.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+
+        if (file) {
+            if (file.type.startsWith('image/')) {
+                const objectURL = URL.createObjectURL(file);
+                image.src = objectURL;
+
+                // Revoke previous URL after loading to free memory
+                image.onload = () => URL.revokeObjectURL(objectURL);
+            } else {
+                display.innerHTML = `File selected: ${file.name}`;
+            }
         }
     });
-});
+}
 
-
-
-// document.getElementById('fileInput').addEventListener('change', function(event) {
-//     const file = event.target.files[0];
-//     const display = document.getElementById('fileDisplay');
-
-//     if (file) {
-//         if (file.type.startsWith('image/')) {
-//             const img = document.createElement('img');
-//             img.src = URL.createObjectURL(file);
-//             display.innerHTML = '';
-//             display.appendChild(img);
-//         } else {
-//             display.innerHTML = `File selected: ${file.name}`;
-//         }
-//     }
-// });
-
-
-document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const display = document.getElementById('fileDisplay');
-
-    if (file) {
-        if (file.type.startsWith('image/')) {
-            const img = document.createElement('img');
-            img.src = URL.createObjectURL(file);
-            display.innerHTML = ''; // Clear previous content
-            display.appendChild(img);
-        } else {
-            display.innerHTML = `File selected: ${file.name}`;
-        }
-    }
-});
-
-document.getElementById('fileInput2').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const display = document.getElementById('fileDisplay2'); 
-    const preImage = document.getElementById('preImage2'); 
-
-    if (file) {
-        if (file.type.startsWith('image/')) {
-            preImage.src = URL.createObjectURL(file); 
-        } else {
-            display.innerHTML = `File selected: ${file.name}`;
-        }
-    }
-});
-
-
-
-
-
+// Apply function to both upload inputs
+handleImageUpload('fileInput', 'fileDisplay', 'preImage');
+handleImageUpload('fileInput2', 'fileDisplay2', 'preImage2');
